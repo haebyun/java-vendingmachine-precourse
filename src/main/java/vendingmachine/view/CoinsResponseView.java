a339ee05a4e0253b88065336253cc2741e92dad3
MingyeomKim <mingyum119@naver.com>
2023-11-26 15:36:57 +0900

    feat: `자판기가 보유한 동전` 메시지를 출력

--- src/main/java/vendingmachine/view/CoinsResponseView.java
+++ src/main/java/vendingmachine/view/CoinsResponseView.java
@@ -1,9 +1,13 @@
 package vendingmachine.view;
 
 import vendingmachine.global.controller.dto.response.CoinsResponse;
+import vendingmachine.view.console.ConsoleWriter;
 
 public class CoinsResponseView {
+    private static final String message = "자판기가 보유한 동전";
+
     public void print(CoinsResponse coinsResponse) {
+        ConsoleWriter.printlnMessage(message);
 
     }
 }
