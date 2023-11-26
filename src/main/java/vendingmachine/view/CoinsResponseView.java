83a3afa0762d57d9b02d28701b2731e1b774e78f
MingyeomKim <mingyum119@naver.com>
2023-11-26 15:49:46 +0900

    refactor: 출력 뷰를 정적 메서드로 변경

--- src/main/java/vendingmachine/view/CoinsResponseView.java
+++ src/main/java/vendingmachine/view/CoinsResponseView.java
@@ -5,16 +5,16 @@ import vendingmachine.global.controller.dto.response.CoinResponse;
 import vendingmachine.global.controller.dto.response.CoinsResponse;
 import vendingmachine.view.console.ConsoleWriter;
 
-public class CoinsResponseView {
+public final class CoinsResponseView {
     private static final String NOTICE = "자판기가 보유한 동전";
     private static final String COIN_RESPONSE_FORMAT = "%d원 - %d개";
 
-    public void print(CoinsResponse coinsResponse) {
+    public static void print(CoinsResponse coinsResponse) {
         ConsoleWriter.printlnMessage(NOTICE);
         printCoinResponses(coinsResponse.responses());
     }
 
-    private void printCoinResponses(List<CoinResponse> responses) {
+    private static void printCoinResponses(List<CoinResponse> responses) {
         responses.forEach(
                 response -> ConsoleWriter.printlnFormat(
                         COIN_RESPONSE_FORMAT,
