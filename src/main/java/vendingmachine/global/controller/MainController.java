0ad03d530b084b3fa9b5bfe9c2575811780caaf4
MingyeomKim <mingyum119@naver.com>
2023-11-26 14:20:47 +0900

    refactor: 모든 기능을 실행하는 클래스 이름을 MainController로 변경

--- src/main/java/vendingmachine/global/controller/MainController.java
+++ src/main/java/vendingmachine/global/controller/MainController.java
@@ -3,13 +3,13 @@ package vendingmachine.global.controller;
 import vendingmachine.global.domain.Amount;
 import vendingmachine.view.AmountInputView;
 
-public class VendingMachine {
+public class MainController {
     public void run() {
         initialize();
     }
 
     private void initialize() {
         Amount payment = Amount.from(AmountInputView.requestAmount());
-
+        
     }
 }
