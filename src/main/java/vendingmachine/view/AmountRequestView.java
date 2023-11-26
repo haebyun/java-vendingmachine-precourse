af498613d504e72eb3ffa127e4f9b9f2833b94b4
MingyeomKim <mingyum119@naver.com>
2023-11-26 14:33:19 +0900

    refactor: 보유 금액을 입력받는 클래스 이름을 AmountRequestView로 수정

--- src/main/java/vendingmachine/view/AmountRequestView.java
+++ src/main/java/vendingmachine/view/AmountRequestView.java
@@ -4,7 +4,7 @@ import vendingmachine.global.validator.Validator;
 import vendingmachine.view.console.ConsoleReader;
 import vendingmachine.view.console.ConsoleWriter;
 
-public final class AmountInputView {
+public final class AmountRequestView {
     private static final String message = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final int DIVISOR = 10;
 
