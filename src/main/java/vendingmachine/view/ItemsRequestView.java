1abb3d0006d34b06d9f23edc15f7d84a265d5cb1
MingyeomKim <mingyum119@naver.com>
2023-11-26 16:19:50 +0900

    feat: 상품 정보를 DTO 객체에 저장하여 전달

--- src/main/java/vendingmachine/view/ItemsRequestView.java
+++ src/main/java/vendingmachine/view/ItemsRequestView.java
@@ -1,30 +1,37 @@
 package vendingmachine.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import vendingmachine.controller.request.ItemRequest;
+import vendingmachine.controller.request.ItemsRequest;
 import vendingmachine.global.exception.CustomException;
 import vendingmachine.global.exception.ErrorMessage;
+import vendingmachine.global.validator.Validator;
 import vendingmachine.view.console.ConsoleReader;
 import vendingmachine.view.console.ConsoleWriter;
 
 public class ItemsRequestView {
     private static final String NOTICE = "상품명과 가격, 수량을 입력해 주세요.";
     private static final String REGEX = "\\[[^,]+,\\d+,\\d+\\]";
-    private static final String SEPARATOR = ";";
+    private static final String ITEMS_SEPARATOR = ";";
+    private static final String ITEM_SEPARATOR = ",";
 
-
-    public static void requestItems() {
+    public static ItemsRequest requestItems() {
         ConsoleWriter.printlnMessage(NOTICE);
-        validate(ConsoleReader.enterMessage());
+        String input = ConsoleReader.enterMessage();
+        List<String> items = validate(input);
+        return toItemsRequest(items);
     }
 
-    private static void validate(String input) {
+    private static List<String> validate(String input) {
         List<String> items = split(input);
         validateFormat(items);
+        return items;
     }
 
     private static List<String> split(String input) {
-        return Arrays.stream(input.split(SEPARATOR))
+        return Arrays.stream(input.split(ITEMS_SEPARATOR))
                 .map(String::trim)
                 .toList();
     }
@@ -43,5 +50,27 @@ public class ItemsRequestView {
         //        Matcher matcher = pattern.matcher(input);
     }
 
+    private static ItemsRequest toItemsRequest(List<String> items) {
+        List<ItemRequest> itemRequests = new ArrayList<>();
+        for (String item : items) {
+            itemRequests.add(toItemRequest(item));
+        }
+        return new ItemsRequest(itemRequests);
+    }
 
+    private static ItemRequest toItemRequest(String input) {
+        String[] parts = input.substring(1, input.length() - 1).split(ITEM_SEPARATOR);
+        String name = parts[0];
+        String price = parts[1];
+        String quantity = parts[2];
+
+        Validator.validatePositiveNumber(price);
+        Validator.validatePositiveNumber(quantity);
+
+        return new ItemRequest(
+                name,
+                Integer.parseInt(price),
+                Integer.parseInt(quantity)
+        );
+    }
 }
