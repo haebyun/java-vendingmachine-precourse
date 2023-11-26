e80cf8debc144bc8dd94136605baa4e24cd4e4fd
MingyeomKim <mingyum119@naver.com>
2023-11-26 15:49:34 +0900

    feat: 잘못된 보유 금액 입력 시 다시 입력받는 기능 구현

--- src/main/java/vendingmachine/global/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/global/controller/VendingMachineController.java
@@ -1,17 +1,33 @@
 package vendingmachine.global.controller;
 
+import java.util.function.Supplier;
 import vendingmachine.global.domain.Amount;
 import vendingmachine.global.domain.VendingMachine;
 import vendingmachine.view.AmountRequestView;
+import vendingmachine.view.CoinsResponseView;
+import vendingmachine.view.console.ConsoleWriter;
 
 public class VendingMachineController {
 
     public void run() {
-        initialize();
+        VendingMachine vendingMachine = initialize();
+        CoinsResponseView.print(vendingMachine.toCoinsResponse());
     }
 
     private VendingMachine initialize() {
-        Amount amount = Amount.from(AmountRequestView.requestAmount());
+        Amount amount = retry(() -> {
+            return Amount.from(AmountRequestView.requestAmount());
+        });
         return VendingMachine.of(amount);
+    }
+
+    private static <T> T retry(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                ConsoleWriter.printlnMessage(e.getMessage());
+            }
+        }
     }
 }
