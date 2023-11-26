291fc5d6fddc315460b2dac1cbd65ff52f01bcc2
MingyeomKim <mingyum119@naver.com>
2023-11-26 16:05:00 +0900

    feat: 올바른 구분자를 통해 상품이 입력되었는 지 검증하는 기능 구현

--- src/main/java/vendingmachine/view/ItemsRequestView.java
+++ src/main/java/vendingmachine/view/ItemsRequestView.java
@@ -1,14 +1,47 @@
 package vendingmachine.view;
 
+import java.util.Arrays;
+import java.util.List;
+import vendingmachine.global.exception.CustomException;
+import vendingmachine.global.exception.ErrorMessage;
 import vendingmachine.view.console.ConsoleReader;
 import vendingmachine.view.console.ConsoleWriter;
 
 public class ItemsRequestView {
     private static final String NOTICE = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String REGEX = "\\[[^,]+,\\d+,\\d+\\]";
+    private static final String SEPARATOR = ";";
 
-    public void requestItems() {
+
+    public static void requestItems() {
         ConsoleWriter.printlnMessage(NOTICE);
-        String message = ConsoleReader.enterMessage();
-        
+        validate(ConsoleReader.enterMessage());
     }
+
+    private static void validate(String input) {
+        List<String> items = split(input);
+        validateFormat(items);
+    }
+
+    private static List<String> split(String input) {
+        return Arrays.stream(input.split(SEPARATOR))
+                .map(String::trim)
+                .toList();
+    }
+
+    private static void validateFormat(List<String> items) {
+        if (hasInvalidFormat(items)) {
+            throw CustomException.from(ErrorMessage.SEPARATOR_ERROR);
+        }
+    }
+
+    private static boolean hasInvalidFormat(List<String> items) {
+        return !items.stream()
+                .allMatch(item -> item.matches(REGEX));
+
+        //    Pattern pattern = Pattern.compile(BEVERAGE_FORMAT_REGEX);
+        //        Matcher matcher = pattern.matcher(input);
+    }
+
+
 }
