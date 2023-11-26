50c1cc4f6e3944a5bc9f5a97975248cc12ed0bb7
MingyeomKim <mingyum119@naver.com>
2023-11-26 14:17:23 +0900

    refactor: Payment 라는 명칭을 Amount로 변경

--- src/main/java/vendingmachine/view/AmountInputView.java
+++ src/main/java/vendingmachine/view/AmountInputView.java
@@ -4,20 +4,20 @@ import vendingmachine.global.validator.Validator;
 import vendingmachine.view.console.ConsoleReader;
 import vendingmachine.view.console.ConsoleWriter;
 
-public class PaymentInputView {
+public final class AmountInputView {
     private static final String message = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final int DIVISOR = 10;
 
-    public int requestPayment() {
+    public static int requestAmount() {
         ConsoleWriter.printlnMessage(message);
-        String payment = ConsoleReader.enterMessage();
-        return validate(payment);
+        String amount = ConsoleReader.enterMessage();
+        return validate(amount);
     }
 
-    private int validate(String input) {
+    private static int validate(String input) {
         Validator.validatePositiveNumber(input);
-        int payment = Integer.parseInt(input);
-        Validator.validateDivisible(payment, DIVISOR);
-        return payment;
+        int amount = Integer.parseInt(input);
+        Validator.validateDivisible(amount, DIVISOR);
+        return amount;
     }
 }
