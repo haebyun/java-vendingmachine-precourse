c8747baf834ed906bec1a8e42c911f8285e55eae
MingyeomKim <mingyum119@naver.com>
2023-11-26 15:39:50 +0900

    feat: 생성한 동전과 그 금액을 출력하는 기능 구현

--- src/main/java/vendingmachine/view/CoinsResponseView.java
+++ src/main/java/vendingmachine/view/CoinsResponseView.java
@@ -1,13 +1,26 @@
 package vendingmachine.view;
 
+import java.util.List;
+import vendingmachine.global.controller.dto.response.CoinResponse;
 import vendingmachine.global.controller.dto.response.CoinsResponse;
 import vendingmachine.view.console.ConsoleWriter;
 
 public class CoinsResponseView {
-    private static final String message = "자판기가 보유한 동전";
+    private static final String NOTICE = "자판기가 보유한 동전";
+    private static final String COIN_RESPONSE_FORMAT = "%d원 - %d개";
 
     public void print(CoinsResponse coinsResponse) {
-        ConsoleWriter.printlnMessage(message);
+        ConsoleWriter.printlnMessage(NOTICE);
+        printCoinResponses(coinsResponse.responses());
+    }
 
+    private void printCoinResponses(List<CoinResponse> responses) {
+        responses.forEach(
+                response -> ConsoleWriter.printlnFormat(
+                        COIN_RESPONSE_FORMAT,
+                        response.amount(),
+                        response.count()
+                )
+        );
     }
 }
