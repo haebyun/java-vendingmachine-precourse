2694796793a0cfedfcba17b802e0f14240c78268
haebyun <bhk4392@gmail.com>
2023-11-26 16:23:25 +0900

    fix: Item의 생성자에 quantity 인자를 삭제

--- src/main/java/vendingmachine/domain/Item.java
+++ src/main/java/vendingmachine/domain/Item.java
@@ -4,7 +4,7 @@ public class Item {
     private final String name;
     private final int price;
 
-    public Item(String name, int price, int quantity) {
+    public Item(String name, int price) {
         this.name = name;
         this.price = price;
     }
