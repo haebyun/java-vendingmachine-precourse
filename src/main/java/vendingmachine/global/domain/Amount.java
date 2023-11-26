54fcdce946c7d09ef7361acaef4f20d3fd763e8d
MingyeomKim <mingyum119@naver.com>
2023-11-26 14:16:52 +0900

    feat(Amount): 자판기가 보유하는 금액을 저장하는 도메인 클래스 생성

---
+++ src/main/java/vendingmachine/global/domain/Amount.java
@@ -0,0 +1,13 @@
+package vendingmachine.global.domain;
+
+public class Amount {
+    private final int amount;
+
+    private Amount(int amount) {
+        this.amount = amount;
+    }
+
+    public static Amount from(int amount) {
+        return new Amount(amount);
+    }
+}
