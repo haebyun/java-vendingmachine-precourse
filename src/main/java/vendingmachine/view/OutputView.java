9a4e647eeac50fc101eda5f8181dada54624bf93
haebyun <bhk4392@gmail.com>
2023-11-28 15:40:54 +0900

    feat(OutputView): 투입 금액을 출력하는 기능 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -6,9 +6,14 @@ public class OutputView {
 
     public void printErrorMessage (String message) {
         System.out.println(message);
+        System.out.println();
     }
 
     public void printAvailableCoins(List<String> coins){
         coins.forEach(System.out::println);
+    }
+
+    public void printAvailableBalance(int balance) {
+        System.out.println("투입 금액: " + balance);
     }
 }
